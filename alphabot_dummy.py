b8b57a51bfce87f0a9861f68dcd1ab2576631fab
Andrea-Tomatis <58698567+Andrea-Tomatis@users.noreply.github.com>
2021-11-17 17:31:38 +0100

    Update alphabot_dummy.py

--- alphabot_dummy.py
+++ alphabot_dummy.py
@@ -1,14 +1,16 @@
 '''
-Stupid version of the Alphabot module
+RaspberryPi Alphabot: test version of the alphabot library
+
+@Andrea-Tomatis
+@Nicolo-Cora
 '''
 
-import time
-
-
+# Copy of the Alphabot class.
+# Every method returns only a string to print
 class AlphaBot(object):
     
     def __init__(self, in1=13, in2=12, ena=6, in3=21, in4=20, enb=26):
-        pass
+        pass # no parameters are needed to make the object run
 
     def forward(self):
         return 'forward'
@@ -25,22 +27,13 @@ class AlphaBot(object):
     def right(self, speed=30):
         return 'turn right'
         
-    def set_pwm_a(self, value):
+    def set_pwm_a(self, value): # simulates the motorA pulse modulation
         return f'set pwm_a to {value}'
 
-    def set_pwm_b(self, value):
+    def set_pwm_b(self, value): # simulates the motorA pulse modulation
         return f'set pwm_b to {value}'    
         
-    def set_motor(self, left, right):
+    def set_motor(self, left, right): #simulates the motors settings
         return 'motors settled'
 
 
-if __name__ == '__main__':
-
-    Ab = AlphaBot()
-    Ab.forward()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        pass
